f3da7e452c32f921fd61ee8d2b2bb5e7c9fc3af1
Reshinth Adithyan <36307201+reshinthadithyan@users.noreply.github.com>
2023-01-11 01:05:17 +0530

    Bug fix

--- pile/filtering/subset_pilev2.py
+++ pile/filtering/subset_pilev2.py
@@ -1,29 +1,33 @@
-from datasets import load_from_disk,load_dataset
+from datasets import load_from_disk
 import multiprocessing as mp
 import os
 import logging
 from tqdm import tqdm
-from transformers import GPTNeoXTokenizerFast
 import argparse
 import time
 import pathlib
-import ast
 import random
 
 logging.basicConfig(
     level = logging.INFO,
     format=  '%(asctime)s %(levelname)-8s %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
-    filename=f"logs/the_pile_subset_{time.time()}.log")
+    filename=f"pile/logs/the_pile_subset_{time.time()}.log")
 logger = logging.getLogger(__name__)
 
-def subset_dataset(dataset,percentage:float=10.0):
+def subset_dataset_fn(dataset,percentage:float=10.0):
     
     percentage_total = int(len(dataset)*percentage/100)
     idxs = random.sample(range(len(dataset)),percentage_total)
+    
+    total_len = len(dataset)
     dataset = dataset.select(idxs)
     #idxs to dataset column
-    dataset["subset_idx"] = idxs
+    print(f"Slicing 10% which is {percentage_total} |  {len(dataset)} out of {total_len}")
+    logger.info(f"Slicing 10% which is {percentage_total} | {len(dataset)} out of {total_len}")
+    dataset.add_column("subset_idx",idxs)
+    print(dataset)
+    logger.info(dataset)
     return dataset
 
 
@@ -39,37 +43,44 @@ if __name__ == "__main__":
 
     logger.info("Loading dataset")
     for sub_dir in tqdm(input_sub_dirs,leave=False):
-        if sub_dir != "non_local_dedup":
-            path_sub_dir = os.path.join(args.input_dir, sub_dir)
-            print(path_sub_dir)
-            logger.info(path_sub_dir)
-            dataset = load_from_disk(path_sub_dir)
-            logger.info(f"Loaded dataset from {path_sub_dir}")
-            print(f"Loaded dataset from {path_sub_dir}")
-            subset_dataset = subset_dataset(dataset,percentage=10.0)
-            logger.info(f"Subsetting dataset to {args.percentage}%")
-
-            logger.info("Saving dataset")
-            output_dir = pathlib.Path(os.path.join(args.output_dir, sub_dir))
-            output_dir.mkdir(parents=True, exist_ok=True)
-            subset_dataset.save_to_disk(args.output_dir)
-            logger.info(f"Saved dataset at {args.output_dir}")
-        else:
-            logger.info(f"The subdir is non_local_dedup")
-            print(f"The subdir is non_local_dedup")
-            non_local_sub_dirs = os.listdir(os.path.join(args.input_dir,sub_dir))
-            for non_local_sub_dir in tqdm(non_local_sub_dirs):
-                path_non_local_sub_dir = os.path.join(args.input_dir,sub_dir, non_local_sub_dir)
+        try:
+            if sub_dir != "non_local_dedup":
+                path_sub_dir = os.path.join(args.input_dir, sub_dir)
                 print(path_sub_dir)
                 logger.info(path_sub_dir)
-                dataset = load_from_disk(path_non_local_sub_dir)
-                logger.info(f"Loaded dataset from {path_non_local_sub_dir}")
-                print(f"Loaded dataset from {path_non_local_sub_dir}")
-                subset_dataset = subset_dataset(dataset,percentage=10.0)
+                dataset = load_from_disk(path_sub_dir)
+                logger.info(f"Loaded dataset from {path_sub_dir}")
+                print(f"Loaded dataset from {path_sub_dir}")
+                subset_dataset = subset_dataset_fn(dataset,percentage=10.0)
                 logger.info(f"Subsetting dataset to {args.percentage}%")
 
                 logger.info("Saving dataset")
-                output_dir = pathlib.Path(os.path.join(args.output_dir,sub_dir ,non_local_sub_dir))
+                output_dir = pathlib.Path(os.path.join(args.output_dir, sub_dir))
                 output_dir.mkdir(parents=True, exist_ok=True)
                 subset_dataset.save_to_disk(args.output_dir)
                 logger.info(f"Saved dataset at {args.output_dir}")
+            else:
+                logger.info(f"The subdir is non_local_dedup")
+                print(f"The subdir is non_local_dedup")
+                non_local_sub_dirs = os.listdir(os.path.join(args.input_dir,sub_dir))
+                for non_local_sub_dir in tqdm(non_local_sub_dirs,leave=False):
+                    path_non_local_sub_dir = os.path.join(args.input_dir,sub_dir, non_local_sub_dir)
+                    print(path_sub_dir)
+                    logger.info(path_sub_dir)
+                    dataset = load_from_disk(path_non_local_sub_dir)
+                    logger.info(f"Loaded dataset from {path_non_local_sub_dir}")
+                    print(f"Loaded dataset from {path_non_local_sub_dir}")
+                    subset_dataset = subset_dataset(dataset,percentage=10.0)
+                    logger.info(f"Subsetting dataset to {args.percentage}%")
+
+                    logger.info("Saving dataset")
+                    output_dir = pathlib.Path(os.path.join(args.output_dir,sub_dir ,non_local_sub_dir))
+                    output_dir.mkdir(parents=True, exist_ok=True)
+                    subset_dataset.save_to_disk(args.output_dir)
+                    logger.info(f"Saved dataset at {args.output_dir}")
+        except PermissionError:
+            logger.info(f"Permission Error at {sub_dir}")
+            print(f"Permission Error at {sub_dir}")
+        except FileNotFoundError:
+            logger.info(f"FileNotFoundError  at {sub_dir}")
+            print(f"FileNotFoundError  at {sub_dir}")
