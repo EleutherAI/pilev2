24813a240f6a3bd0be616b48663ad4fde5fbb4e1
Reshinth Adithyan <36307201+reshinthadithyan@users.noreply.github.com>
2023-01-18 21:51:22 +0530

    Fix log path Shard script

--- pile/processing/split_shards.py
+++ pile/processing/split_shards.py
@@ -9,7 +9,7 @@ logging.basicConfig(
     level = logging.INFO,
     format=  '%(asctime)s %(levelname)-8s %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
-    filename=f"logs/stats_{time.time()}_split_group.log")
+    filename=f"logs/stats_{time.time()}_split_shards.log")
 
 logger = logging.getLogger(__name__)
 
